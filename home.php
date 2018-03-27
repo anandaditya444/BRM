ab3ff2c589b18ff464d103e41075f265d763cfad
Md Shahid <36432967+dshahid380@users.noreply.github.com>
2018-03-27 21:32:58 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    	<title>Home Page</title>
+    </head>
+    <body>
+    	<h1>Book Records Management</h1>
+    	<a href="insertForm.php">Insert Records</a><br/>
+    	<a href="view.php">View Records</a><br/>
+    	<a href="deleteForm.php">Delete Records</a><br/>
+    	<a href="updateForm.php">Updation Records</a><br/>
+    	
+
+    </body>
+</html>
---
+++ view.php
@@ -0,0 +1,47 @@
+<?php
+ $con=mysqli_connect('127.0.0.1','root');
+ mysqli_select_db($con,'BRM_DB');
+ $q="select * from book";
+ $result=mysqli_query($con,$q);
+ $num=mysqli_num_rows($result);
+ mysqli_close($con);
+
+?>
+
+<!DOCTYPE html>
+<html>
+   <head>
+   	  <title>View Book Records</title>
+   	  <link rel="stylesheet" href="viewStyle.css"/>
+   </head>
+   <body>
+   	  <h1>Book Records Management</h1>
+   	  <table id="view-table">
+   	  	<tr>
+   	  		<th>Book ID</th>
+   	  		<th>Title</th>
+   	  		<th>Price</th>
+   	  		<th>Author</th>
+
+   	  	</tr>
+   	  	<?php
+   	  	  for($i=1;$i<=$num;$i++)
+   	  	  { $row=mysqli_fetch_array($result);
+   	  	  	?>
+   	  	  	<tr>
+   	  	  		<td><?php echo $row['bookid']; ?></td>
+   	  	  		<td><?php echo $row['title']; ?></td>
+   	  	  		<td><?php echo $row['price']; ?></td>
+   	  	  		<td><?php echo $row['author']; ?></td>
+   	  	  	</tr>
+   	  	  	<?php 
+
+   	  	   } 
+
+   	  	  	
+   	  	?>
+   	  </table>
+   	  Go back to homepage <a href="home.php">Click Here</a>
+   </body>
+
+</html>
